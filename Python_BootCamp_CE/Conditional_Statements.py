0c82b8a5890f334bbe113a84e4ad817c3cacdcd2
RUPLALLD <RUPLALLD@coned.com>
2019-07-19 11:41:59 -0400

    Adding new files

---
+++ Python_BootCamp_CE/Conditional_Statements.py
@@ -0,0 +1,14 @@
+# Control flow in Python
+# if
+# elif
+# else
+# The use of colon and white space is key in Python
+
+hungry = False
+
+if hungry == True:
+    print('Its true')
+elif hungry == False:
+    print('False')
+else:
+    print('This will never run bro!')
