a0ca6c2b161a444c32472fc6e9ef13aff82d2754
RUPLALLD <RUPLALLD@CE015069783057>
2019-08-09 08:48:09 -0400

    Remove C

--- Python_BootCamp_CE/Class_Polymorphism.py
+++ Python_BootCamp_CE/Class_Polymorphism.py
@@ -40,7 +40,7 @@ class Animal():
     def speak(self):
         raise NotImplementedError('Subclass must implement this abstract method')
 
-c
+
 
 # This would fail as we are trying to instantiate an abstract class
 #my_animal = Animal('Dog')
